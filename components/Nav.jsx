6f98f61d3dbdf0fba93e819c9d63e3ee30a9be4c
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2023-07-23 13:23:33 +0300

    component: Create Desktop navigation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,17 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+
+    setProviders();
+  }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -27,9 +38,35 @@ const Nav = () => {
             <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile picture"
+              />
+            </Link>
           </div>
         ) : (
-          <div></div>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => {
+                <button
+                  type="button"
+                  key={provider.key}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>;
+              })}
+          </>
         )}
       </div>
     </nav>
